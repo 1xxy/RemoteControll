31bd762fc5c60509cff9abc86e80dfe0d152c8e2
xu xiangyun <65609288+1xxy@users.noreply.github.com>
2022-02-26 23:54:23 +0800

    Update control.c

--- control.c
+++ control.c
@@ -22,7 +22,7 @@
 #define MSG_TYPE_COMMD_LIST_CLIENT              (6)
 #define MSG_TYPE_COMMD_LIST_CLIENT_RESULT       (7)
 
-#define SERVER_IP                           "139.196.120.117"
+#define SERVER_IP                           "xxx.xxx.xxx.xxx" //公网ip
 #define SERVER_PORT                         8090
 
 #define LOGIN_TYPE_CLIENT                   (0)
